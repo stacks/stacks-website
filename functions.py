9c82e029cbcf32cba66d7d080b207cc2a6bd4e61
Pieter Belmans <pieterbelmans@gmail.com>
2012-06-07 17:17:42 +0200

    Relate TeX files to titles

---
+++ functions.py
@@ -0,0 +1,30 @@
+import os
+
+# return all files with a given extension
+def list_files(path, extension):
+  return filter(lambda filename: filename.endswith(extension), os.listdir(path))
+
+# return all TeX files in a directory
+def list_tex_files(path):
+  return list_files(path, '.tex')
+
+# get the title from a TeX file
+def get_title(path):
+  tex_file = open(path, 'r')
+
+  title = ''
+  for line in tex_file:
+    if line.startswith('\\title'):
+      title = line[7:-2]
+      break
+
+  return title
+
+# get the dictionary relating TeX files to their respective titles
+def get_titles(path):
+  titles = {}
+
+  for tex_file in list_tex_files(path):
+    titles[tex_file] = get_title(path + tex_file)
+
+  return titles
