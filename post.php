9248f096b9393013c191a41188f0366bb778c4a0
Pieter Belmans <pieterbelmans@gmail.com>
2012-06-30 22:02:55 +0200

    Preliminary check for empty name / email

--- post.php
+++ post.php
@@ -13,6 +13,17 @@
 
   // TODO all kinds of validation and escaping
 
+  // empty author
+  if (empty($_POST['name'])) {
+    print('You should supply your name.');
+    exit();
+  }
+  // empty email
+  if (empty($_POST['email'])) {
+    print('You should supply your email address.');
+    exit();
+  }
+
   // from here on it's safe to ignore the fact that it's user input
   $tag = $_POST['tag'];
   $author = $_POST['name'];
