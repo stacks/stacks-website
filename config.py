d563a0372836ac7b23976eef8f4631e9ed6b870f
Pieter Belmans <pieterbelmans@gmail.com>
2012-07-02 10:54:03 +0200

    Started using a config.py script

---
+++ config.py
@@ -0,0 +1,7 @@
+# current test version on paard.math.columbia.edu
+#directory = '/~pieter/algebraic_geometry/stacks-website/'
+# local version
+directory = '/'
+
+def full_url(path):
+  return directory + path
