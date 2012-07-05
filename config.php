ef50a85b31903844238a5dc1e4e880a19b3f0c99
Pieter Belmans <pieterbelmans@gmail.com>
2012-07-05 09:08:18 -0400

    We can remove the direction and domain configuration now

--- config.php
+++ config.php
@@ -1,13 +1,6 @@
 <?php
-  // current test version on paard.math.columbia.edu
-  //$directory = '/~pieter/algebraic_geometry/stacks-website/';
-  //$domain = 'http://paard.math.columbia.edu:8080'
-  // local version
-  $directory = '/';
-  $domain = 'localhost';
-
   // place the database in a directory that is not visible from outside
-  $database_directory = '../';
+  $database_directory = '';
   $database_name = 'stacks.sqlite';
 
   function get_database_location() {
