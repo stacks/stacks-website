426f3e2b4b21f6f263634958cfd98d298c114d94
Pieter Belmans <pieterbelmans@gmail.com>
2012-07-05 10:37:23 +0200

    It seems better to use htmlspecialchars

--- tag.php
+++ tag.php
@@ -173,13 +173,13 @@
       foreach ($comments as $comment) {
         print("    <div class='comment'>\n");
         print("      <a name='comment-" . $comment['id'] . "'></a>\n");
-        print("      Comment by <cite class='comment-author'>" . htmlentities($comment['author']) . "</cite>");
+        print("      Comment by <cite class='comment-author'>" . htmlspecialchars($comment['author']) . "</cite>");
         if (!empty($comment['site'])) {
           print(" (<a href='" . $comment['site'] . "'>site</a>)\n");
         }
         $date = date_create($comment['date'], timezone_open('GMT'));
         print("      <span class='comment-date'><a href='#comment-" . $comment['id'] . "'>" . date_format($date, 'F j, Y \a\t g:i a e') . "</a></span>\n");
-        print("      <blockquote>" . str_replace("\xA0", ' ', Markdown(htmlentities($comment['comment']))) . "</blockquote>\n");
+        print("      <blockquote>" . str_replace("\xA0", ' ', Markdown(htmlspecialchars($comment['comment']))) . "</blockquote>\n");
         print("    </div>\n\n");
       }
     }
