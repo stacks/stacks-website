2f802733e23945214ee413b6be55d93d44c0a7e0
Pieter Belmans <pieterbelmans@gmail.com>
2012-07-05 09:31:39 +0200

    Implemented a global database location and automatic path detection for the URL's

--- config.php
+++ config.php
@@ -6,9 +6,21 @@
   $directory = '/';
   $domain = 'localhost';
 
+  // place the database in a directory that is not visible from outside
+  $database_directory = '../';
+  $database_name = 'stacks.sqlite';
+
+  function get_database_location() {
+    global $database_directory, $database_name;
+    return 'sqlite:' . $database_directory . $database_name;
+  }
+
   
   function full_url($path) {
-    global $directory;
-    return $directory . $path;
+    $directory = $_SERVER['SCRIPT_NAME'];
+    explode('/', $directory);
+    $directory = implode('/', array_splice($directory, -1));
+
+    return $directory . '/' . $path;
   }
 ?>
