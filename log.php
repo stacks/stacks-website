903c008abe170b915ff7825fa2acd48258199df0
Pieter Belmans <pieterbelmans@gmail.com>
2012-07-10 17:35:31 +0200

    Fixed the missing log.log file

---
+++ log.php
@@ -0,0 +1,17 @@
+<?php
+header('Content-type: text/plain');
+
+$output = '';
+exec('git submodule foreach git log --stat -50', $output);
+
+$tex_project = false;
+foreach ($output as $line) {
+  if (substr($line, 0, 9) == 'Entering ') {
+    $tex_project = ($line == 'Entering \'tex\'');
+    continue;
+  }
+
+  if ($tex_project)
+    print($line . "\n");
+}
+?>
