90f502f5f9bdb2b5ebccbf9e079609dd51a01591
Pieter Belmans <pieterbelmans@gmail.com>
2012-07-10 11:56:43 +0200

    New database format (tags can contain a name now)

--- database.sql
+++ database.sql
@@ -1,3 +1,3 @@
 CREATE TABLE "comments" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE , "tag" VARCHAR NOT NULL, "author" VARCHAR, "site" VARCHAR, "date" DATETIME DEFAULT CURRENT_TIMESTAMP, "comment" TEXT)
 CREATE TABLE "sections" ("number" VARCHAR PRIMARY KEY  NOT NULL ,"title" VARCHAR,"filename" VARCHAR NOT NULL )
-CREATE TABLE "tags" ("tag" VARCHAR PRIMARY KEY  NOT NULL ,"label" VARCHAR,"file" VARCHAR,"chapter_page" INTEGER,"book_page" INTEGER,"type" VARCHAR,"book_id" VARCHAR,"value" TEXT, "active" BOOL NOT NULL  DEFAULT TRUE)
+CREATE TABLE "tags" ("tag" VARCHAR PRIMARY KEY  NOT NULL ,"label" VARCHAR,"file" VARCHAR,"chapter_page" INTEGER,"book_page" INTEGER,"type" VARCHAR,"book_id" VARCHAR,"value" TEXT, "active" BOOL NOT NULL  DEFAULT TRUE, "name" VARCHAR)
