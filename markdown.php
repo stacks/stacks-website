731ed77b15b91ee3b7c36c5c79baa6fbddf50260
Pieter Belmans <pieterbelmans@gmail.com>
2012-07-15 20:35:51 +0200

    Improved Markdown explanation

--- markdown.php
+++ markdown.php
@@ -38,19 +38,15 @@ Some math: $\varphi\colon X\to Y$, see Tag \ref{04FW}.
   a^2+b^2=c^2
 \end{equation}
 </code></pre>
-    just works. This is done using <a href="http://mathjax.org">MathJax</a> (so you need to enable Javascript).
-
-    <p>Remark that underscores are <strong>automatically escaped</strong>, so adding emphasis <code>_like this_</code> <strong>does not work</strong>. This in order to let MathJax and Markdown cooperate nicely. You can use the functionally equivalent asterisks <code>*like this*</code>.
-
-    <p>MathJax also supports equation numbering through <code>\label{}</code> and <code>\ref{}</code> or <code>\eqref{}</code>, so you can number your equations and refer to them. This brings us to the next subject: Stacks Flavored Markdown.
+    <p>just works. This is done using <a href="http://mathjax.org">MathJax</a> (so you need to enable Javascript).</p>
 
     <h2>Stacks Flavored Markdown</h2>
-    <p>Inspired by <a href="http://github.github.com/github-flavored-markdown/">GitHub Flavored Markdown</a> which is an extension of ordinary Markdown by things belonging specifically to GitHub (like referencing commits, users, etc.) we have created Stacks Flavored Markdown, or <abbr title="Stacks Flavored Markdown">SFM</abbr>.
+    <p>Inspired by <a href="http://github.github.com/github-flavored-markdown/">GitHub Flavored Markdown</a> which is an extension of ordinary Markdown by things belonging specifically to GitHub (like referencing commits, users, etc.) we have created <em>Stacks Flavored Markdown</em>, or <abbr title="Stacks Flavored Markdown">SFM</abbr>.
 
     <p>An overview:
 
     <ol>
-      <li>As discussed before you can use LaTeX the way you are used to.</li>
+      <li>As discussed before you can use LaTeX-style mathematics the way you are used to.</li>
 
       <li>You can refer to tags by using <code>\ref{tag}</code>. So <code>\ref{0001}</code> will automatically be converted to <a href="<?php print(full_url('tag/0001')); ?>"><var>0001</var></a>. For this to trigger you just need <code>tag</code> to be a valid tag, it doesn't necessarily have to exist in the database.
 
@@ -59,7 +55,7 @@ Some math: $\varphi\colon X\to Y$, see Tag \ref{04FW}.
         <p>Remark that it is <em>not encouraged</em> to use this type of referencing: tags are stable, labels are not. 
     </ol>
 
-    <p>Note that only LaTeX works in the preview mode, uses of <code>\ref{}</code> will not be rendered.
+    <p>Note that not all references to tags can be parsed in preview mode, only <em>references to tags will be parsed in the preview</em>. Others will be read by MathJax which will result in the well known ?? from LaTeX.
 
     <h2>Remarks</h2>
     <p>The combination of Markdown, LaTeX and <abbr>SFM</abbr> probably contains bugs. Several external tools are needed for this setup to work, and there can be scenarios where it fails to produce the correct output. Also we can't guarantee that the preview looks exactly like your final comment.
