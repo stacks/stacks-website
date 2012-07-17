d0ae29b64f1f0d89e541baacf68d5c1d410ce068
Aise Johan de Jong <aise.johan.de.jong@gmail.com>
2012-07-16 21:42:05 -0400

    Add --prefix=stacks-project/ to git archive command

--- scripts/update.py
+++ scripts/update.py
@@ -11,4 +11,4 @@ if not os.path.exists("../archives"):
   os.system("mkdir ../archives")
 os.system("tar -c -f ../archives/stacks-pdfs.tar --exclude book.pdf --transform=s@tex/tags/tmp@stacks-pdfs@ ../tex/tags/tmp/*.pdf")
 os.system("tar -c -f ../archives/stacks-dvis.tar --exclude book.dvi --transform=s@tex/tags/tmp@stacks-dvis@ ../tex/tags/tmp/*.dvi")
-os.system("git archive HEAD --remote=../tex/ | bzip2 > ../archives/stacks-project.tar.bz2")
+os.system("git archive HEAD --prefix=stacks-project/ --remote=../tex/ | bzip2 > ../archives/stacks-project.tar.bz2")
