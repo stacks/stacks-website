57c0b695785810e0bfaff66560562164e0cecb5b
Pieter Belmans <pieterbelmans@gmail.com>
2012-09-23 17:23:34 +0200

    Very preliminary tag preview (i.e. parsing the LaTeX)

--- functions.php
+++ functions.php
@@ -167,6 +167,97 @@ function parse_preview($preview) {
   return $preview;
 }
 
+function parse_references($string) {
+  // look for \ref before MathJax can and see if they point to existing tags
+  $references = array();
+
+  preg_match_all('/\\\ref{[\w-]*}/', $string, $references);
+  foreach ($references[0] as $reference) {
+    // get the label or tag we're referring to, nothing more
+    $target = substr($reference, 5, -1);
+
+    // we're referring to a tag
+    if (is_valid_tag($target)) {
+      // regardless of whether the tag exists we insert the link, the user is responsible for meaningful content
+      $string = str_replace($reference, '[`' . $target . '`](' . full_url('tag/' . $target) . ')', $string);
+    }
+    // the user might be referring to a label
+    else {
+      // might it be that he is referring to a "local" label, i.e. in the same chapter as the tag?
+      if (!label_exists($target)) {
+        $label = get_label(strtoupper($_GET['tag']));
+        $parts = explode('-', $label);
+        // let's try it with the current chapter in front of the label
+        $target = $parts[0] . '-' . $target;
+      }
+
+      // the label (potentially modified) exists in the database (and it is active), so the user is probably referring to it
+      // if he declared a \label{} in his string with this particular label value he's out of luck
+      if (label_exists($target)) {
+        $tag = get_tag_referring_to($target);
+        $string = str_replace($reference, '[`' . $tag . '`](' . full_url('tag/' . $tag) . ')', $string);
+      }
+    }
+  }
+
+  return $string;
+}
+
+function parse_latex($code) {
+  // remove labels
+  $code = preg_replace("/\\\label\{.*\}/", "", $code);
+
+  // all big environments with their corresponding markup
+  $code = str_replace("\\begin{lemma}\n", "<strong>Lemma</strong> <em>", $code);
+  $code = str_replace("\\end{lemma}\n", "</em></p>", $code);
+
+  // proof environment
+  $code = str_replace("\\begin{proof}\n", "<p><strong>Proof</strong> ", $code);
+  $code = str_replace("\\end{proof}\n", "$\square$</p>", $code);
+
+  /**
+   * TODO
+   * - {\it ...}
+   * ``
+   * better reference parsing
+   * better environment parsing
+   * \section etc.
+   * \square in right corner of proof
+   */
+
+  // enumerates etc.
+  $code = str_replace("\\begin{enumerate}\n", "<ol>", $code);
+  $code = str_replace("\\end{enumerate}\n", "</ol>", $code);
+  $code = str_replace("\\item", "<li>", $code);
+
+  // let HTML be aware of paragraphs
+  $code = str_replace("\n\n", "</p><p>", $code);
+
+  // parse references
+  $code = preg_replace('/\\\ref\{(.*)\}/', "$1", $code);
+
+  // remove \medskip and \noindent
+  $code = str_replace("\\medskip", "", $code);
+  $code = str_replace("\\noindent", "", $code);
+
+  // fix macros
+  $macros = array(
+    // TODO check \mathop in output
+    "\\lim" => "\mathop{\\rm lim}\\nolimits",
+    "\\colim" => "\mathop{\\rm colim}\\nolimits",
+    "\\Spec" => "\mathop{\\rm Spec}",
+    "\\Hom" => "\mathop{\\rm Hom}\\nolimits",
+    "\\SheafHom" => "\mathop{\mathcal{H}\!{\it om}}\nolimits",
+    "\\Sch" => "\\textit{Sch}",
+    "\\Mor" => "\mathop{\\rm Mor}\\nolimits",
+    "\\Ob" => "\mathop{\\rm Ob}\\nolimits",
+    "\\Sh" => "\mathop{\\textit{Sh}}\\nolimits");
+
+  $code = str_replace(array_keys($macros), array_values($macros), $code);
+
+  return $code;
+}
+
 function print_navigation() {
 ?>
     <ul id="navigation">
@@ -181,5 +272,10 @@ function print_navigation() {
     </ul>
     <br style="clear: both;">
 <?php
+}
+
+function print_tag_code_and_preview($code) {
+  print("    <pre class='tag-preview-code'>\n" . parse_preview($code) . "\n    </pre>\n");
+  print("    <blockquote class='tag-preview-'>\n" . parse_latex($code) . "</blockquote>\n");
 }
 ?>
