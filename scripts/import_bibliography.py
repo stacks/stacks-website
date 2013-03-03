8ef709d02dbff53538910583801e333aed63e24f
Aise Johan de Jong <aise.johan.de.jong@gmail.com>
2013-03-03 12:14:01 -0500

    Simplify the logic in import_bibliography.py
    Maybe it is just me but I couldn't parse it before. I've simplified it
    by requiring the key = value pairs to be on a line. Namely, the check
    for ending values wasn't correct anyway! (As curly braces can occur in
    the text of a value --- of course the checks in the script don't catch
    all errors even now.)

--- scripts/import_bibliography.py
+++ scripts/import_bibliography.py
@@ -30,11 +30,20 @@ def import_bibliography(location):
   f = open(location)
 
   items = []
-  not_finished = False
+  in_item = 0
+  line_nr = 0
 
   for line in f:
+    line_nr = line_nr + 1
     # beginning of a new item
     if line[0] == '@':
+
+      if in_item == 1:
+        print "On line:", line_nr, "of my.bib:"
+	print line
+        print "Nested items. Exiting."
+	exit(1)
+
       # clear previous item
       item = [[], {}]
 
@@ -43,36 +52,49 @@ def import_bibliography(location):
 
       item[0] = (bib_type, name)
 
+      in_item = 1
+
       continue
   
     # end of an item
     if line[0] == '}':
+
+      if in_item == 0:
+        print "On line:", line_nr, "of my.bib:"
+        print line
+	print "Nested items. Exiting."
+	exit(1)
+
       # add a *copy* to the list of items
       items.append(list(item))
 
+      in_item = 0
+
+      continue
+
+    # can ignore current line if not in an item
+    if in_item == 0:
+
       continue
  
-    # check whether we're still building a value or not
-    if not_finished:
-      # append to current value
-      value = value + ' ' + line.strip().strip(',')
-      # now it's finished
-      if line.strip()[-2:] == '},' or line.strip()[-2:] == '",' or line.strip()[-1] == '}' or line.strip()[-1:] == '"':
-        not_finished = False
-        item[1][key] = value
+    # ignore comments
+    if line[0] == '%' or line[0:2] == '//':
 
-    else:
-      if '=' in line:
-        key = line.partition('=')[0].strip().lower()
+      continue
 
-        if line.strip()[-2:] == '},' or line.strip()[-2:] == '",' or line.strip()[-1] == '}' or line.strip()[-1:] == '"':
-          value = line.partition('=')[2].strip().strip(',')[1:-1]
-          item[1][key] = value
+    # Get key value pair from the line if it contains = sign
+    if '=' in line:
+      key = line.partition('=')[0].strip().lower()
+      value = line.partition('=')[2].strip().strip(',')[1:-1]
+      item[1][key] = value
 
-        else:
-          not_finished = True
-          value = line.partition('=')[2].strip()
-  
+      # Check for correctness
+      if not (line.strip()[-2:] == '},' or line.strip()[-2:] == '",' or line.strip()[-1] == '}' or line.strip()[-1:] == '"'):
+        print "Warning: run over on line:", line_nr, "in my.bib:"
+        print line
+        print "Should not happen: each key = value pair should be on a line!"
+	print "Exiting"
+	exit(1)
 
   for item in items:
     insert_item(item)
