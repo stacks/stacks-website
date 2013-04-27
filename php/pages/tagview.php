81d1cf0aaa4c633192e27ed5d0343d12adaff57b
Pieter Belmans <pieterbelmans@gmail.com>
2013-04-27 14:20:39 +0200

    Fixed static type in citations
    Not everything is a section, obviously...

--- php/pages/tagview.php
+++ php/pages/tagview.php
@@ -139,9 +139,9 @@ class TagViewPage extends Page {
     $value .= "or one of the following (click to see and copy the code)";
     $value .= "<ul id='citation-options'>";
     $value .= "<li><a href='javascript:copyToClipboard(\"\\cite[\\href{http://stacks.math.columbia.edu/tag/" . $this->tag["tag"] . "}{Tag " . $this->tag["tag"] . "}]{stacks-project}\")'>[Tag " . $this->tag["tag"] . ", Stacks]</a>";
-    $value .= "<li><a href='javascript:copyToClipboard(\"\\cite[\\href{http://stacks.math.columbia.edu/tag/" . $this->tag["tag"] . "}{Section " . $this->tag["tag"] . "}]{stacks-project}\")'>[Section " . $this->tag["tag"] . ", Stacks]</a>";
+    $value .= "<li><a href='javascript:copyToClipboard(\"\\cite[\\href{http://stacks.math.columbia.edu/tag/" . $this->tag["tag"] . "}{" . ucfirst($this->tag["type"]) . " " . $this->tag["tag"] . "}]{stacks-project}\")'>[" . ucfirst($this->tag["type"]) . " " . $this->tag["tag"] . ", Stacks]</a>";
     $value .= "<li><a href='javascript:copyToClipboard(\"\\href{http://stacks.math.columbia.edu/tag/" . $this->tag["tag"] . "}{Tag " . $this->tag["tag"] . "}\")'>Tag " . $this->tag["tag"] . "</a>";
-    $value .= "<li><a href='javascript:copyToClipboard(\"\\href{http://stacks.math.columbia.edu/tag/" . $this->tag["tag"] . "}{Tag " . $this->tag["tag"] . "}\")'>Section " . $this->tag["tag"] . "</a>";
+    $value .= "<li><a href='javascript:copyToClipboard(\"\\href{http://stacks.math.columbia.edu/tag/" . $this->tag["tag"] . "}{" . ucfirst($this->tag["type"]) . " " . $this->tag["tag"] . "}\")'>" . ucfirst($this->tag["type"]) . " " . $this->tag["tag"] . "</a>";
     $value .= "</ul>";
     $value .= "<p>For more information, see <a href='#'>How to reference tags</a>.</p>";
 
