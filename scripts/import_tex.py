e090610f6b2d1b4ebf4a9a5548c61322148a3141
Pieter Belmans <pieterbelmans@gmail.com>
2013-06-05 14:03:48 -0400

    Import location in the TeX file in the database

--- scripts/import_tex.py
+++ scripts/import_tex.py
@@ -13,56 +13,63 @@ be wise to:
 """
 
 # Only use this script after running
-#	make tags
+#   make tags
 #
 def parse_aux_file(name, path):
-	label_loc = {}
-	aux_file = open(path + "tags/tmp/" + name + ".aux", 'r')
-	for line in aux_file:
-		if line.find("\\newlabel{") < 0:
-			continue
-		
-		n = find_sub_clause(line, 9, "{", "}")
-		short_label = line[10: n]
+    label_loc = {}
+    aux_file = open(path + "tags/tmp/" + name + ".aux", 'r')
+    for line in aux_file:
+        if line.find("\\newlabel{") < 0:
+            continue
+        
+        n = find_sub_clause(line, 9, "{", "}")
+        short_label = line[10: n]
 
-		if short_label.find("tocindent") == 0:
-			continue
+        if short_label.find("tocindent") == 0:
+            continue
 
-		# Turn short label into full label if necessary
-		if not name == "book":
-			label = name + "-" + short_label
-		else:
-			label = short_label
-		
-		split = split_label(label)
+        # Turn short label into full label if necessary
+        if not name == "book":
+            label = name + "-" + short_label
+        else:
+            label = short_label
+        
+        split = split_label(label)
 
-		# Find the current number of the lemma, theorem, etc.
-		m = find_sub_clause(line, n + 2, "{", "}")
-		nr = line[n + 3: m]
+        # Find the current number of the lemma, theorem, etc.
+        m = find_sub_clause(line, n + 2, "{", "}")
+        nr = line[n + 3: m]
 
-		# find the current page number
-		k = find_sub_clause(line, m + 1, "{", "}")
-		page = line[m + 2: k]
+        # find the current page number
+        k = find_sub_clause(line, m + 1, "{", "}")
+        page = line[m + 2: k]
 
-		text = cap_type(split[1]) + " " + nr + " on page " + page
-		label_loc[label] = text
-	aux_file.close()
-	return label_loc
+        text = cap_type(split[1]) + " " + nr + " on page " + page
+        label_loc[label] = text
+    aux_file.close()
+    return label_loc
 
 # Variable to contain all the texts of labels
 label_texts = {}
+
+# Variable to contain all the line numbers related to labels
+label_linenumbers = {}
 
 # Variable to contain all the texts of proofs
 proof_texts = {}
 
 # Helper function
 def assign_label_text(label, text):
-	if not label:
-		exit(1)
-	if not text:
-		print label
-		exit(1)
-	label_texts[label] = text
+    if not label:
+        exit(1)
+    if not text:
+        print label
+        exit(1)
+    label_texts[label] = text
+
+def assign_label_linenumbers(label, linenumbers):
+    print "label " + label + " is on lines " + str(linenumbers)
+    label_linenumbers[label] = list(linenumbers)
 
 path = '../tex/'
 
@@ -76,224 +83,274 @@ lijstje = list_text_files(path)
 
 # Get locations in chapters
 list_dict = {}
+lijstje.remove("perfect") # TODO fix this, conflicting versions of output require this for testing purposes
+lijstje.remove("spaces-divisors")
 for name in lijstje:
-	label_loc = parse_aux_file(name, path)
-	list_dict[name] = label_loc
+    label_loc = parse_aux_file(name, path)
+    list_dict[name] = label_loc
 
 # get locations in book
 label_loc = parse_aux_file("book", path)
 
 # Function to convert refs into links
 def make_links(line, name):
-	new_line = ""
-	m = 0
-	n = line.find("\\ref{")
-	while n >= 0:
-		new_line = new_line + line[m : n + 5]
-		m = find_sub_clause(line, n + 4, "{", "}")
-		ref = line[n + 5: m]
-		if standard_label(ref):
-			label = name + "-" + ref
-		else:
-			label = ref
-		if label in label_tags:
-			new_line = new_line + '<a href="' + config.full_url('tag/' + label_tags[label]) + '\">' + ref + '</a>'
-		else:
-			new_line = new_line + ref
-		n = line.find("\\ref{", m)
-	new_line = new_line + line[m:]
-	return new_line
+    new_line = ""
+    m = 0
+    n = line.find("\\ref{")
+    while n >= 0:
+        new_line = new_line + line[m : n + 5]
+        m = find_sub_clause(line, n + 4, "{", "}")
+        ref = line[n + 5: m]
+        if standard_label(ref):
+            label = name + "-" + ref
+        else:
+            label = ref
+        if label in label_tags:
+            new_line = new_line + '<a href="' + config.full_url('tag/' + label_tags[label]) + '\">' + ref + '</a>'
+        else:
+            new_line = new_line + ref
+        n = line.find("\\ref{", m)
+    new_line = new_line + line[m:]
+    return new_line
 
 # Get text of
-#	labeled environments
-#	proofs
-#	labeled items
-#	sections, subsections, subsubsections
-#	equations
+#   labeled environments
+#   proofs
+#   labeled items
+#   sections, subsections, subsubsections
+#   equations
 ext = ".tex"
 for name in lijstje:
-	filename = path + name + ext
-	tex_file = open(filename, 'r')
-	line_nr = 0
-	verbatim = 0
-	in_env = 0
-	in_proof = 0
-	in_item = 0
-	in_section = 0
-	in_subsection = 0
-	in_subsubsection = 0
-	in_equation = 0
-	label = ""
-	label_env = ""
-	label_proof = ""
-	label_item = ""
-	label_section = ""
-	label_subsection = ""
-	label_subsubsection = ""
-	label_equation = ""
-	text_env = ""
-	text_proof = ""
-	text_item = ""
-	text_section = ""
-	text_subsection = ""
-	text_subsubsection = ""
-	text_equation = ""
-	for line in tex_file:
+    filename = path + name + ext
+    tex_file = open(filename, 'r')
+    line_nr = 0
+    verbatim = 0
+    in_env = 0
+    in_proof = 0
+    in_item = 0
+    in_section = 0
+    in_subsection = 0
+    in_subsubsection = 0
+    in_equation = 0
+    label = ""
+    label_env = ""
+    label_proof = ""
+    label_item = ""
+    label_section = ""
+    label_subsection = ""
+    label_subsubsection = ""
+    label_equation = ""
+    text_env = ""
+    text_proof = ""
+    text_item = ""
+    text_section = ""
+    text_subsection = ""
+    text_subsubsection = ""
+    text_equation = ""
 
-		# Update line number
-		line_nr = line_nr + 1
+    linenumber_env = [1, 1]
+    linenumber_proof = [1, 1]
+    linenumber_item = [1, 1]
+    linenumber_section = [1, 1]
+    linenumber_subsection = [1, 1]
+    linenumber_subsubsection = [1, 1] # TODO do these even exist?
+    linenumber_equation = [1, 1]
 
-		# Check for verbatim, because we do not check correctness
-		# inside verbatim environment.
-		verbatim = verbatim + beginning_of_verbatim(line)
-		if verbatim:
-			if end_of_verbatim(line):
-				verbatim = 0
-			continue
+    for line in tex_file:
 
-		# See if labeled environment starts
-		if labeled_env(line) and line.find("\\begin{equation}") < 0:
-			in_env = 1
+        # Update line number
+        line_nr = line_nr + 1
 
-		# See if a proof starts
-		if beginning_of_proof(line):
-			in_proof = 1
+        # Check for verbatim, because we do not check correctness
+        # inside verbatim environment.
+        verbatim = verbatim + beginning_of_verbatim(line)
+        if verbatim:
+            if end_of_verbatim(line):
+                verbatim = 0
+            continue
 
-		# See if item starts
-		if new_item(line):
-			# Closeout previous item
-			if in_item and label_item:
-				assign_label_text(label_item, text_item)
-				in_item = 0
-				text_item = ""
-				label_item = ""
-			in_item = 1
+        # See if labeled environment starts
+        if labeled_env(line) and line.find("\\begin{equation}") < 0:
+            in_env = 1
+            linenumber_env[0] = line_nr
 
-		# See if section starts
-		if new_part(line):
-			# Closeout previous section/subsection/subsubsection
-			if in_section and line.find('\\section') == 0:
-				assign_label_text(label_section, text_section)
-				in_section = 0
-				text_section = ""
-				label_section = ""
-			if in_subsection and (line.find('\\section') == 0 or line.find('\\subsection') == 0):
-				assign_label_text(label_subsection, text_subsection)
-				in_subsection = 0
-				text_subsection = ""
-				label_subsection = ""
-			if in_subsubsection and (line.find('\\section') == 0 or line.find('\\subsection') == 0 or line.find('\\subsubsection') == 0):
-				assign_label_text(label_subsubsection, text_subsubsection)
-				in_subsubsection = 0
-				text_subsubsection = ""
-				label_subsubsection = ""
-			# Start new section/subsection/subsubsection
-			if line.find('\\section') == 0:
-				in_section = 1
-			if line.find('\\subsection') == 0:
-				in_subsection = 1
-			if line.find('\\subsubsection') == 0:
-				in_subsubsection = 1
+        # See if a proof starts
+        if beginning_of_proof(line):
+            in_proof = 1
 
-		# See if equation starts
-		if line.find('\\begin{equation}') == 0:
-			in_equation = 1
+        # See if item starts
+        if new_item(line):
+            # Closeout previous item
+            if in_item and label_item:
+                assign_label_text(label_item, text_item)
+                linenumber_item[1] = line_nr
+                assign_label_linenumbers(label_item, linenumber_item)
 
-		# Find label if there is one
-		if line.find('\\label{') == 0:
-			label = find_label(line)
-			if label.find('item') == 0:
-				label_item = name + '-' + label
-			elif label.find('section') == 0:
-				label_section = name + '-' + label
-			elif label.find('subsection') == 0:
-				label_subsection = name + '-' + label
-			elif label.find('subsubsection') == 0:
-				label_subsubsection = name + '-' + label
-			elif label.find('equation') == 0:
-				label_equation = name + '-' + label
-			else:
-				label_env = name + '-' + label
-				if label.find('lemma') == 0 or label.find('proposition') == 0 or label.find('theorem') == 0:
-					label_proof = label_env
+                in_item = 0
+                text_item = ""
+                label_item = ""
 
-		# Add line to env_text if we are in an environment
-		if in_env:
-			text_env = text_env + make_links(line, name)
+            in_item = 1
+            linenumber_item[0] = line_nr
 
-		# Add line to proof_text if we are in a proof
-		if in_proof:
-			text_proof = text_proof + make_links(line, name)
+        # See if section starts
+        if new_part(line):
+            # Closeout previous section/subsection/subsubsection
+            if in_section and line.find('\\section') == 0:
+                assign_label_text(label_section, text_section)
+                linenumber_section[1] = line_nr - 1
+                assign_label_linenumbers(label_section, linenumber_section)
 
-		# Add line to item_text if we are in an item
-		if in_item:
-			text_item = text_item + make_links(line, name)
+                in_section = 0
+                text_section = ""
+                label_section = ""
+            if in_subsection and (line.find('\\section') == 0 or line.find('\\subsection') == 0):
+                assign_label_text(label_subsection, text_subsection)
+                linenumber_subsection[1] = line_nr - 1
+                assign_label_linenumbers(label_subsection, linenumber_subsection)
 
-		# Add line to section_text if we are in a section
-		if in_section:
-			text_section = text_section + make_links(line, name)
-		if in_subsection:
-			text_subsection = text_subsection + make_links(line, name)
-		if in_subsubsection:
-			text_subsubsection = text_subsubsection + make_links(line, name)
+                in_subsection = 0
+                text_subsection = ""
+                label_subsection = ""
+            if in_subsubsection and (line.find('\\section') == 0 or line.find('\\subsection') == 0 or line.find('\\subsubsection') == 0):
+                assign_label_text(label_subsubsection, text_subsubsection)
+                linenumber_subsubsection[1] = line_nr - 1
+                assign_label_linenumbers(label_subsubsection, linenumber_subsubsection)
 
-		# Add line to equation_text if we are in an equation
-		if in_equation:
-			text_equation = text_equation + make_links(line, name)
+                in_subsubsection = 0
+                text_subsubsection = ""
+                label_subsubsection = ""
+            # Start new section/subsection/subsubsection
+            if line.find('\\section') == 0:
+                in_section = 1
+                linenumber_section[0] = line_nr
 
-		# Closeout env
-		if end_labeled_env(line) and line.find("\\end{equation}") < 0:
-			in_env = 0
-			assign_label_text(label_env, text_env)
-			text_env = ""
-			label_env = ""
+            if line.find('\\subsection') == 0:
+                in_subsection = 1
+                linenumber_subsection[0] = line_nr
 
-		# Closeout proof
-		if end_of_proof(line):
-			in_proof = 0
-			# We pick up only the first proof if there are multiple proofs
-			if label_proof:
-				if not text_proof:
-					exit(1)
-				proof_texts[label_proof] = text_proof
-			text_proof = ""
-			label_proof = ""
+            if line.find('\\subsubsection') == 0:
+                in_subsubsection = 1
+                linenumber_subsubsection[0] = line_nr
 
-		# Closeout item
-		if line.find('\\end{enumerate}') == 0 or line.find('\\end{itemize}') == 0 or line.find('\\end{list}') == 0:
-			if in_item and label_item:
-				assign_label_text(label_item, text_item)
-				label_item = ""
-			in_item = 0
-			text_item = ""
-			label_item = ""
+        # See if equation starts
+        if line.find('\\begin{equation}') == 0:
+            linenumber_equation[0] = line_nr
+            in_equation = 1
 
-		# Closeout section/subsection/subsubsection
-		if line.find('\\input{chapters}') == 0:
-			if in_section:
-				assign_label_text(label_section, text_section)
-				in_section = 0
-				text_section = ""
-				label_section = ""
-			if in_subsection:
-				assign_label_text(label_subsection, text_subsection)
-				in_subsection = 0
-				text_subsection = ""
-				label_subsection = ""
-			if in_subsubsection:
-				assign_label_text(label_subsubsection, text_subsubsection)
-				in_subsubsection = 0
-				text_subsubsection = ""
-				label_subsubsection = ""
+        # Find label if there is one
+        if line.find('\\label{') == 0:
+            label = find_label(line)
+            if label.find('item') == 0:
+                label_item = name + '-' + label
+            elif label.find('section') == 0:
+                label_section = name + '-' + label
+            elif label.find('subsection') == 0:
+                label_subsection = name + '-' + label
+            elif label.find('subsubsection') == 0:
+                label_subsubsection = name + '-' + label
+            elif label.find('equation') == 0:
+                label_equation = name + '-' + label
+            else:
+                label_env = name + '-' + label
+                if label.find('lemma') == 0 or label.find('proposition') == 0 or label.find('theorem') == 0:
+                    label_proof = label_env
 
-		# Closeout equation
-		if line.find('\\end{equation}') == 0:
-			in_equation = 0
-			assign_label_text(label_equation, text_equation)
-			text_equation = ""
-			label_equation = ""
+        # Add line to env_text if we are in an environment
+        if in_env:
+            text_env = text_env + make_links(line, name)
 
-	tex_file.close()
+        # Add line to proof_text if we are in a proof
+        if in_proof:
+            text_proof = text_proof + make_links(line, name)
+
+        # Add line to item_text if we are in an item
+        if in_item:
+            text_item = text_item + make_links(line, name)
+
+        # Add line to section_text if we are in a section
+        if in_section:
+            text_section = text_section + make_links(line, name)
+        if in_subsection:
+            text_subsection = text_subsection + make_links(line, name)
+        if in_subsubsection:
+            text_subsubsection = text_subsubsection + make_links(line, name)
+
+        # Add line to equation_text if we are in an equation
+        if in_equation:
+            text_equation = text_equation + make_links(line, name)
+
+        # Closeout env
+        if end_labeled_env(line) and line.find("\\end{equation}") < 0:
+            assign_label_text(label_env, text_env)
+            linenumber_env[1] = line_nr # this is not perfect, as we do not take the proof into account, but it should be clear from the context where the proof is to be found
+            assign_label_linenumbers(label_env, linenumber_env)
+
+            in_env = 0
+            text_env = ""
+            label_env = ""
+
+        # Closeout proof
+        if end_of_proof(line):
+            in_proof = 0
+            # We pick up only the first proof if there are multiple proofs
+            if label_proof:
+                if not text_proof:
+                    exit(1)
+                proof_texts[label_proof] = text_proof
+            text_proof = ""
+            label_proof = ""
+
+        # Closeout item
+        if line.find('\\end{enumerate}') == 0 or line.find('\\end{itemize}') == 0 or line.find('\\end{list}') == 0:
+            if in_item and label_item:
+                assign_label_text(label_item, text_item)
+                linenumber_item[1] = line_nr
+                assign_label_linenumbers(label_item, linenumber_item)
+
+                label_item = ""
+            in_item = 0
+            text_item = ""
+            label_item = ""
+
+        # Closeout section/subsection/subsubsection
+        if line.find('\\input{chapters}') == 0:
+            if in_section:
+                assign_label_text(label_section, text_section)
+                linenumber_section[1] = line_nr - 1
+                assign_label_linenumbers(label_section, linenumber_section)
+
+                in_section = 0
+                text_section = ""
+                label_section = ""
+            if in_subsection:
+                assign_label_text(label_subsection, text_subsection)
+                linenumber_subsection[1] = line_nr - 1
+                assign_label_linenumbers(label_subsection, linenumber_subsection)
+
+                in_subsection = 0
+                text_subsection = ""
+                label_subsection = ""
+            if in_subsubsection:
+                assign_label_text(label_subsubsection, text_subsubsection)
+                linenumber_subsubsection[1] = line_nr - 1
+                assign_label_linenumbers(label_subsubsection, linenumber_subsubsection)
+
+                in_subsubsection = 0
+                text_subsubsection = ""
+                label_subsubsection = ""
+
+        # Closeout equation
+        if line.find('\\end{equation}') == 0:
+            in_equation = 0
+            assign_label_text(label_equation, text_equation)
+            linenumber_equation[1] = line_nr
+            assign_label_linenumbers(label_equation, linenumber_equation)
+
+            text_equation = ""
+            label_equation = ""
+
+    tex_file.close()
 
 
 # ZZZZ is a special case
@@ -333,6 +390,15 @@ def extract_proofs(text):
       in_proof = False
 
   return result
+
+def update_linenumbers(tag, linenumbers):
+  # insert the text of the tag in the real table
+  try:
+    query = 'UPDATE tags SET begin = ?, end = ? WHERE tag = ?'
+    connection.execute(query, (linenumbers[0], linenumbers[1], tag))
+
+  except sqlite3.Error, e:
+    print "An error occurred:", e.args[0]
 
 def update_text(tag, text):
   # insert the text of the tag in the real table
@@ -378,6 +444,7 @@ print 'Adding the TeX code to both tables'
 n = 0
 while n < len(tags):
   tag = tags[n][0]
+  print tag
   label = tags[n][1]
   if not label in label_loc:
     print "Warning: missing location for tag", tag
@@ -388,6 +455,9 @@ while n < len(tags):
   name = split[0]
 
   text = ''
+
+  if label in label_linenumbers.keys():
+    update_linenumbers(tag, label_linenumbers[label])
 
   if label in label_texts:
     text = text + label_texts[label]
