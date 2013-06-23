d91a55baf77aca7c023b97f89095d29d1dfa25fd
Pieter Belmans <pieterbelmans@gmail.com>
2013-06-23 16:21:08 +0200

    Change tag to upper case in lookup

--- php/redirects/lookup.php
+++ php/redirects/lookup.php
@@ -2,5 +2,5 @@
   $config = parse_ini_file("../../config.ini");
 
   require_once("../general.php");
-  header("Location: " . $_SERVER["HTTP_ORIGIN"] . href("tag/" . $_POST["tag"]));
+  header("Location: " . $_SERVER["HTTP_ORIGIN"] . href("tag/" . strtoupper($_POST["tag"])));
 ?>
