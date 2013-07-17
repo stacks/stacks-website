d853dac915737fca5d640bf1b0fd2d80b6b8202e
Pieter Belmans <pieterbelmans@gmail.com>
2013-07-17 09:34:48 +0200

    Forgot to commit js/search.js

--- js/search.js
+++ js/search.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
   $("input#keywords").focus();
 
   // insert collapse / expand all links
-  $("#results").before("<a href='javascript:void(0)' onclick='$(\"#results pre\").hide();'> <img src='js/jquery-treeview/images/minus.gif'> Collapse all</a>");
-  $("#results").before(" <a href='javascript:void(0)' onclick='$(\"#results pre\").show();'><img src='js/jquery-treeview/images/plus.gif'> Expand all</a>");
+  $("div#allResults").before("<a href='javascript:void(0)' onclick='$(\"ul.results pre\").hide();'> <img src='js/jquery-treeview/images/minus.gif'> Collapse all</a>");
+  $("div#allResults").before(" <a href='javascript:void(0)' onclick='$(\"ul.results pre\").show();'><img src='js/jquery-treeview/images/plus.gif'> Expand all</a>");
 
   // insert toggle links for each result
-  pre = $("#results pre");
+  pre = $("ul.results pre");
   for (var i = 0; i < pre.length; i++) {
     el = $(pre[i]);
     el.prev().before('<span class="preview"><a href="javascript:void(0)" onclick="$(\'#' + pre[i].id + '\').toggle();">preview</a>');
