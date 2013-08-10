f8dda194403d84fbf4d5461b339efeb32a5225bc
Pieter Belmans <pieterbelmans@gmail.com>
2013-08-10 10:23:57 +0200

    Implemented metadata in the API
    Basically, all the information from getTag that is meaningful to the
    user is given. Including statement, in LaTeX (with some HTML
    interspersed unfortunately).

---
+++ php/api/meta.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once("../tags.php");
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// read configuration file
+$config = parse_ini_file("../../config.ini");
+
+// initialize the global database object
+try {
+  $database = new PDO("sqlite:" . "../../" . $config["database"]);
+}
+catch(PDOException $e) {
+  echo $e->getMessage();
+}
+
+if (!isValidTag($_GET["tag"])) {
+  print "This tag is not valid.";
+  exit;
+}
+
+if (!tagExists($_GET["tag"])) {
+  print "This tag does not exist.";
+  exit;
+}
+
+$tag = getTag($_GET["tag"]);
+
+$result = array();
+$result["type"] = $tag["type"];
+$result["label"] = $tag["label"];
+$result["chapter_page"] = $tag["chapter_page"];
+$result["book_page"] = $tag["book_page"];
+$result["book_id"] = $tag["book_id"];
+$result["value"] = $tag["value"];
+
+print json_encode($result);
+
+?>
