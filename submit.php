a7a3518cb3773ba08419a6ff2c235bbfe0f86f72
Pieter Belmans <pieterbelmans@gmail.com>
2014-07-19 22:20:48 +0200

    Started the PHP code
    This is just a placeholder actually

---
+++ submit.php
@@ -0,0 +1,9 @@
+<?php
+if (isset($_POST["skip"])) {
+  // get a new tag
+  // = request an existing tag from the API and forward the user to a URL like <domain>/sloganerator/0123
+}
+elseif (isset($_POST["submit"])) {
+  // submit the slogan
+}
+?>
