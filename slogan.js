02be91577b734790bafab7afc227a7c20f7b1934
Pieter Belmans <pieterbelmans@gmail.com>
2014-07-19 22:24:17 +0200

    Removed the site field
    I guess this is just wasted input here

--- slogan.js
+++ slogan.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  var fields = ["name", "email", "site"];
+  var fields = ["name", "email"];
   for (var i = 0; i < fields.length; i++) {
     field = fields[i];
     if (localStorage[field]) $("#" + field).val(localStorage[field]);
