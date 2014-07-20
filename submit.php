bcc85cde24337ca8c9d4544632385c605dcc174b
Pieter Belmans <pieterbelmans@gmail.com>
2014-07-20 14:48:09 +0200

    Requesting new tags is done by `input.php`
    We just send the user to the file `input.php`, which will request a random tag.

--- submit.php
+++ submit.php
@@ -1,7 +1,6 @@
 <?php
 if (isset($_POST["skip"])) {
-  // get a new tag
-  // = request an existing tag from the API and forward the user to a URL like <domain>/sloganerator/0123
+  header("Location: input.php");
 }
 elseif (isset($_POST["submit"])) {
   // submit the slogan
