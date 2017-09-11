324c12fb75ddd33059592da49081b82825b07e54
Pieter Belmans <pieterbelmans@gmail.com>
2017-09-11 10:23:48 +0200

    Johan's blog doesn't work with HTTPS

--- php/config.php
+++ php/config.php
@@ -5,8 +5,8 @@
     "jQuery" => "https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js",
     "MathJax" => "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js",
     "D3" => "https://d3js.org/d3.v3.min.js",
-    "blog" => "https://math.columbia.edu/~dejong/wordpress/",
-    "blog feed" => "https://math.columbia.edu/~dejong/wordpress/?feed=rss2",
+    "blog" => "http://math.columbia.edu/~dejong/wordpress/",
+    "blog feed" => "http://math.columbia.edu/~dejong/wordpress/?feed=rss2",
     "GitHub feed" => "https://github.com/stacks/stacks-project/commits/master.atom",
     "SimplePie cache" => $_SERVER["DOCUMENT_ROOT"] . "/php/cache",
   );
